814fed301b8ad7efce90b5299e863417c129edbc
Mohamed Alaa <40716185+mohamedalaam@users.noreply.github.com>
2021-12-15 14:36:49 +0200

    .

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an unofficial implementation of motion inbetweening paper',
+    author='Mohamed Alaa',
+    license='',
+)
